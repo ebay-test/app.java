9d6b56c18ebfa0b6256bd438aaab79c9feaf28d8
engrcatherine <adebowalecatherine@gmail.com>
2025-06-05 15:11:42 -0500

    Your commit message

---
+++ app.java
@@ -0,0 +1,6 @@
+"please enter your username"
+ read username 
+ if [ $username -e database.sql ] then:
+ echo "continue" 
+ echo "please enter your password"
+ echo "sorry you entered an invalid username"
